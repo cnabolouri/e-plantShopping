a9c924c841f1a10774d5f2b8df9207ad690c6d18
Sina Bolouri <cnabolouri@gmail.com>
2025-10-19 21:35:22 -0700

    o

--- e-plantShopping/src/components/Navbar.jsx
+++ e-plantShopping/src/components/Navbar.jsx
@@ -8,10 +8,10 @@ export default function Navbar() {
     return (
         <header className="nav">
             <nav className="nav-inner">
-                <NavLink to="" className="brand">Paradise Nursery</NavLink>
+                <NavLink to="/e-plantShopping" className="brand">Paradise Nursery</NavLink>
                 <div className="links">
-                    <NavLink to="plants">Plants</NavLink>
-                    <Link to="cart" className="cart-link">
+                    <NavLink to="/e-plantShopping/plants">Plants</NavLink>
+                    <Link to="/e-plantShopping/cart" className="cart-link">
                         <span className="cart-icon">🛒</span>
                         <span className="cart-count">{count}</span>
                     </Link>
