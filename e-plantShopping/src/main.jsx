b37c29c4fcf5c717bf5c10ec362a6b251d765bd8
Sina Bolouri <cnabolouri@gmail.com>
2025-10-19 21:39:33 -0700

    a

--- e-plantShopping/src/main.jsx
+++ e-plantShopping/src/main.jsx
@@ -10,7 +10,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <Provider store={store}>
-            <BrowserRouter basename="/e-plantShopping">
+            <BrowserRouter basename="/e-plant">
                 <App />
             </BrowserRouter>
         </Provider>
